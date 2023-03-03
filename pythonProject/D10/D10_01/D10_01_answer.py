4af53b90eddd991aec0b75f10e6eab6b85a5db2f
Hyunjin Kim <hyunjin.k1994@gmail.com>
2023-03-03 11:06:30 +0900

    등산로 조정

--- pythonProject/D10/D10_01/D10_01_answer.py
+++ pythonProject/D10/D10_01/D10_01_answer.py
@@ -1,19 +1,3 @@
-# [제약 사항]
-#
-# 1. 시간 제한 : 최대 51개 테스트 케이스를 모두 통과하는 데 C/C++/Java 모두 3초
-#
-# 2. 지도의 한 변의 길이 N은 3 이상 8 이하의 정수이다. (3 ≤ N ≤ 8)
-#
-# 3. 최대 공사 가능 깊이 K는 1 이상 5 이하의 정수이다. (1 ≤ K ≤ 5)
-#
-# 4. 지도에 나타나는 지형의 높이는 1 이상 20 이하의 정수이다.
-#
-# 5. 지도에서 가장 높은 봉우리는 최대 5개이다.
-#
-# 6. 지형은 정수 단위로만 깎을 수 있다.
-#
-# 7. 필요한 경우 지형을 깎아 높이를 1보다 작게 만드는 것도 가능하다.
-
 def checkLoad(map_li, i,j, sum, answer):
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
@@ -52,19 +36,6 @@ for case in range(1,T+1):
     N, K = map(int,input().split())
     map_li = [list(map(int,input().split())) for _ in range(N)]
 
-    # load_list = []
-    # for i in range(N):
-    #     part_list = []
-    #     for j in range(N):
-    #         load_max = max(checkLoad(map_li,i,j,1,[1]))
-    #         part_list.append(load_max)
-    #     load_list.append(part_list)
-    # print(load_list)
-
-    # for i in range(N):
-    #     for j in range(N):
-    #         for m in range(K):
-
     max_height_list = findXY(map_li)
     answer_list = []
     answer = 0
@@ -82,4 +53,4 @@ for case in range(1,T+1):
                 map_li[a][b] = tmp
 
 
-    print(answer)
+    print("#{} {}".format(case,answer))
